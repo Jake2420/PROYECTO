f4eaf83d33e535b3afb665b0733060136e52bd35
MathiasMendozaOviedo <137976920+Jake2420@users.noreply.github.com>
2025-01-18 14:22:05 -0500

    Update IIMARPE_Interface_git.py

--- IIMARPE_Interface_git.py
+++ IIMARPE_Interface_git.py
@@ -198,7 +198,7 @@ if submit_button and query:
 
                 except Exception as e:
                     log_and_display_error(f"Error al realizar la consulta: {e}\n{traceback.format_exc()}")
-        else:
-            log_and_display_error("La base de datos no está cargada. Por favor, procesa nuevos archivos o verifica la carga de la base de datos persistente.")
+            else:
+                log_and_display_error("La base de datos no está cargada. Por favor, procesa nuevos archivos o verifica la carga de la base de datos persistente.")
         else:
             st.warning("La pregunta ya fue enviada.")**
