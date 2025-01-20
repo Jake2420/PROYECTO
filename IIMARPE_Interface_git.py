f96322d64739d426766bced0ff2a1b5a8b69dcae
MathiasMendozaOviedo <137976920+Jake2420@users.noreply.github.com>
2025-01-20 11:41:37 -0500

    Update IIMARPE_Interface_git.py

--- IIMARPE_Interface_git.py
+++ IIMARPE_Interface_git.py
@@ -12,7 +12,6 @@ from streamlit_chat import message
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import Document
 import streamlit as st
-import chromadb
 import pdfplumber
 import time
 __import__('pysqlite3')
