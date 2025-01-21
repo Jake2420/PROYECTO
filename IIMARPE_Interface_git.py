b6b7d3b625f4d16a94a85751981810f2d00f1b98
MathiasMendozaOviedo <137976920+Jake2420@users.noreply.github.com>
2025-01-21 01:42:12 -0500

    Update IIMARPE_Interface_git.py

--- IIMARPE_Interface_git.py
+++ IIMARPE_Interface_git.py
@@ -74,7 +74,6 @@ if "last_query" not in st.session_state:
 try:
     if st.session_state.vectorstore is None:
         def PersistentClient(path: str = "./vectordb",
-                             settings: Optional[Settings] = None,
                              tenant: str = 'DEFAULT_TENANT',
                              database: str = 'DEFAULT_DATABASE'):
             # Devuelve el cliente con configuración adecuada
